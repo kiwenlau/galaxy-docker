fd17814681e7595bb9d444fa202ff1dc091e9d5f
kiwenlau <kiwenlau@126.com>
2016-02-20 16:26:19 +0900

    galaxy 15.10.1

--- install-galaxy.sh
+++ install-galaxy.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+GALAXY_VERSION=15.10.1
+
 # Install vim
 apt-get update
 apt-get install -y vim wget python
 
-# Install Galaxy 15.07
-wget https://github.com/galaxyproject/galaxy/archive/v15.07.tar.gz
-tar -xzvf v15.07.tar.gz
-rm v15.07.tar.gz
-mv galaxy-15.07 galaxy
+# Install Galaxy 
+wget https://github.com/galaxyproject/galaxy/archive/v$GALAXY_VERSION.tar.gz
+tar -xzvf v$GALAXY_VERSION.tar.gz
+rm v$GALAXY_VERSION.tar.gz
+mv galaxy-$GALAXY_VERSION galaxy
 
 sed 's/^#host = 127.0.0.1/host = 0.0.0.0/' /root/galaxy/config/galaxy.ini.sample > /root/galaxy/config/galaxy.ini
 sed -i "s@^#file_path = database/files@file_path = /opt/workdir/galaxy_files@" /root/galaxy/config/galaxy.ini
