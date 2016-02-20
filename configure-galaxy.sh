ecf2f34365172dfa5363dcff27af1696507603a2
kiwenlau <kiwenlau@126.com>
2016-02-20 20:50:19 +0900

    set workdir

--- configure-galaxy.sh
+++ configure-galaxy.sh
@@ -12,6 +12,8 @@ mysql -uroot -e "SET PASSWORD FOR galaxy@localhost=PASSWORD('galaxy');"
 # configure galaxy
 sed 's/^#host = 127.0.0.1/host = 0.0.0.0/' /root/galaxy/config/galaxy.ini.sample > /root/galaxy/config/galaxy.ini
 sed -i "s@^#file_path = database/files@file_path = /opt/workdir/galaxy_files@" /root/galaxy/config/galaxy.ini
+sed -i "s@^#new_file_path = database/tmp@new_file_path = /opt/workdir/database/tmp@" galaxy/config/galaxy.ini
+sed -i "s@^#job_working_directory = database/job_working_directory@job_working_directory = /opt/workdir/database/job_working_directory@" galaxy/config/galaxy.ini
 sed -i 's$^#database_connection = sqlite:///./database/universe.sqlite?isolation_level=IMMEDIATE$database_connection = mysql://galaxy:galaxy@localhost:3306/galaxy?unix_socket=/var/run/mysqld/mysqld.sock$' /root/galaxy/config/galaxy.ini
 sed -i 's/^#database_engine_option_pool_recycle = -1/database_engine_option_pool_recycle = 7200/' /root/galaxy/config/galaxy.ini
 sed -i 's$^#tool_dependency_dir = None$tool_dependency_dir = ../tool_dependency$' /root/galaxy/config/galaxy.ini
